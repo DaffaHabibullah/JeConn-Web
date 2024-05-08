869994a864b0a1cbf1ed60428fab6e9cf5dd699c
DaffaHabibullah <daffahabibullah1@gmail.com>
2024-05-08 22:08:11 +0700

    feat: navbar and footer, user api

---
+++ frontend/src/api/User/index.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import { API_URL, AuthHeader } from '../Config';
+
+export const fetchUserProfile = async () => {
+    try {
+        const response = await axios.get(`${API_URL}/user/profile`, {
+            headers: AuthHeader()
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Fetch user error:', error);
+        throw error;
+    }
+};
+
+export const fetchUserUpdate = async (fullName, dateOfBirth, gender, address, phoneNumber) => {
+    try {
+        const response = await axios.post(`${API_URL}/user/update-profile`, {
+            fullName,
+            dateOfBirth,
+            gender,
+            address,
+            phoneNumber
+        }, {
+            headers: AuthHeader()
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Update user error:', error);
+        throw error;
+    }
+};
+
+export const fetchUserUpdateImage = async (imageProfile) => {
+    try {
+        const formData = new FormData();
+        formData.append('imageProfile', imageProfile);
+        const response = await axios.post(`${API_URL}/user/update-image-profile`, formData, {
+            headers: {
+                ...AuthHeader(),
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Update user image error:', error);
+        throw error;
+    }
+};
+
+export const fetchProfileImage = async (id, filename) => {
+    try {
+        const response = await axios.get(`${API_URL}/user/${id}/profile/${filename}`);
+        return response.data;
+    } catch (error) {
+        console.error('Fetch profile image error:', error);
+        throw error;
+    }
+};
---
+++ frontend/src/components/Footer/index.jsx
@@ -0,0 +1,25 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
+const FooterComponent = () => {
+    return (
+        <div style={{ marginTop: '64px', paddingTop: '32px', borderTop: '1px solid #00A47F' }}>
+            <Container>
+                <Row>
+                    <Col className="text-center">
+                        <img className="d-inline-block align-top" src="/jeconn.png" alt="Logo Image" width="30" height="30" />
+                        <span className="d-flex align-items-center justify-content-center"><b>JECONN</b></span>
+                    </Col>
+                </Row>
+                <hr />
+                <Row className="mt-3">
+                    <Col className="text-center">
+                        <span>&copy; 2024 JECONN. All rights reserved.</span>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+
+export default FooterComponent;

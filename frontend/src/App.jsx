2d6015db0897503b702c930e078fcde434fc35b5
DaffaHabibullah <daffahabibullah1@gmail.com>
2024-05-10 00:12:19 +0700

    feat: user profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
+import User from './pages/User';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
+        <Route path='/user-profile' element={<User />} />
       </Routes>
     </Router>
   )
---
+++ frontend/src/pages/User/index.jsx
@@ -0,0 +1,164 @@
+import { useState, useEffect } from 'react';
+import { Container, Row, Col, Card, Form, InputGroup, Badge, Button } from 'react-bootstrap';
+import NavbarComponent from '../../components/Navbar';
+import { fetchUserProfile, fetchUserUpdate, fetchUserUpdateImage } from '../../api/User';
+
+const User = () => {
+    const [userProfile, setUserProfile] = useState({
+        imageProfile: '',
+        fullName: '',
+        dateOfBirth: '',
+        gender: '',
+        address: '',
+        phoneNumber: '',
+        roles: []
+    });
+    const [isEditMode, setIsEditMode] = useState(false);
+    const token = localStorage.getItem('token');
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const response = await fetchUserProfile();
+                setUserProfile(response.data);
+            } catch (error) {
+                console.error('Failed to fetch user profile:', error);
+                throw error;
+            }
+        };
+
+        if (token) {
+            fetchUser();
+        }
+    }, [token]);
+
+    const handleInputChange = (e) => {
+        const { id, value } = e.target;
+        setUserProfile(prevState => ({
+            ...prevState,
+            [id]: value
+        }));
+    };
+
+    const handleConfirmClick = async () => {
+        try {
+            await fetchUserUpdate(
+                userProfile.fullName,
+                userProfile.dateOfBirth,
+                userProfile.gender,
+                userProfile.address,
+                userProfile.phoneNumber
+            );
+            setIsEditMode(false);
+        } catch (error) {
+            console.error('Failed to update user profile:', error);
+        }
+    };
+
+    const handleImageChange = async (e) => {
+        const selectedImage = e.target.files[0];
+        if (selectedImage) {
+            try {
+                await fetchUserUpdateImage(selectedImage);
+                setUserProfile(prevState => ({
+                    ...prevState,
+                    imageProfile: URL.createObjectURL(selectedImage)
+                }));
+            } catch (error) {
+                console.error('Failed to update profile image:', error);
+            }
+        }
+    };
+
+    const handleImageClick = () => {
+        document.getElementById('imageInput').click();
+    };
+
+    const genderOptions = [
+        { label: "Laki-laki", value: "Laki-laki" },
+        { label: "Perempuan", value: "Perempuan" },
+    ];
+
+    return (
+        <div>
+            <NavbarComponent />
+            <Container style={{ paddingTop: '32px' }}>
+                {userProfile && (
+                    <Row md={2} className="d-flex justify-content-center align-items-center">
+                        <Card className="d-flex flex-column align-items-center" style={{ padding: '16px' }}>
+                            <Card.Img
+                                variant="top"
+                                style={{ width: '192px', height: '192px', padding: '8px', borderRadius: '50%', cursor: 'pointer' }}
+                                src={userProfile.imageProfile}
+                                alt="Profile Image"
+                                onClick={handleImageClick}
+                            />
+                            <input id="imageInput" type="file" accept=".jpg, .jpeg, .png" style={{ display: 'none' }} onChange={handleImageChange} />
+                            <Card.Body className="d-flex flex-column align-items-center">
+                                <Card.Title><b>{userProfile.username}</b></Card.Title>
+                                <Card.Text>
+                                    <Row>
+                                        <InputGroup className="mt-3">
+                                            <InputGroup.Text id="full-name" style={{ width: '160px' }}>Nama Lengkap</InputGroup.Text>
+                                            <Form.Control id="fullName" type="text" aria-describedby="full-name" value={userProfile.fullName} onChange={handleInputChange} disabled={!isEditMode} />
+                                        </InputGroup>
+
+                                        <InputGroup className="mt-3">
+                                            <InputGroup.Text id="date-of-birth" style={{ width: '160px' }}>Tanggal Lahir</InputGroup.Text>
+                                            <Form.Control id="dateOfBirth" type="date" aria-describedby="date-of-birth" value={userProfile.dateOfBirth} onChange={handleInputChange} disabled={!isEditMode} />
+                                        </InputGroup>
+
+                                        <InputGroup className="mt-3">
+                                            <InputGroup.Text id="gender" style={{ width: '160px' }}>Jenis Kelamin</InputGroup.Text>
+                                            <Form.Select id="gender" aria-describedby="gender" value={userProfile.gender} onChange={handleInputChange} disabled={!isEditMode}>
+                                                {genderOptions.map((option) => (
+                                                    <option key={option.value} value={option.value}>{option.label}</option>
+                                                ))}
+                                            </Form.Select>
+                                        </InputGroup>
+
+                                        <InputGroup className="mt-3">
+                                            <InputGroup.Text id="address" style={{ width: '160px' }}>Alamat</InputGroup.Text>
+                                            <Form.Control id="address" as="textarea" aria-describedby="address" value={userProfile.address} onChange={handleInputChange} disabled={!isEditMode} />
+                                        </InputGroup>
+
+                                        <InputGroup className="mt-3">
+                                            <InputGroup.Text id="phone-number" style={{ width: '160px' }}>Nomor Telepon</InputGroup.Text>
+                                            <Form.Control id="phoneNumber" type="number" aria-describedby="phone-number" value={userProfile.phoneNumber} onChange={handleInputChange} disabled={!isEditMode} />
+                                        </InputGroup>
+
+                                        <InputGroup className="mt-3 d-flex justify-content-center align-items-center">
+                                            <InputGroup.Text id="roles">Roles</InputGroup.Text>
+                                            {userProfile.roles.map((roles, index) => (
+                                                <Badge key={index} bg={roles === "talent" ? "warning" : "success"} className="me-1">
+                                                    {roles}
+                                                </Badge>
+                                            ))}
+                                        </InputGroup>
+                                    </Row>
+                                </Card.Text>
+                                {isEditMode ? (
+                                    <Row style={{ marginTop: '32px' }}>
+                                        <Col className='d-flex justify-content-center'>
+                                            <Button variant="secondary" onClick={() => setIsEditMode(false)}>Cancel</Button>
+                                            <Button variant="success" className="ms-2" onClick={handleConfirmClick}>Confirm</Button>
+                                        </Col>
+                                    </Row>
+                                ) : (
+                                    <Row style={{ marginTop: '32px' }}>
+                                        <Col className='d-flex justify-content-center'>
+                                            <Button variant="success" onClick={() => setIsEditMode(true)}>Edit Profile</Button>
+                                        </Col>
+                                    </Row>
+                                )}
+                            </Card.Body>
+                        </Card>
+                    </Row>
+                )}
+            </Container>
+        </div>
+    );
+};
+
+
+export default User;

1ee68c36f54da748e88d1a06aa794f545ea30757
DaffaHabibullah <daffahabibullah1@gmail.com>
2024-05-11 01:02:38 +0700

    fix: background color, etc

--- frontend/src/pages/User/index.jsx
+++ frontend/src/pages/User/index.jsx
@@ -89,80 +89,78 @@ const User = () => {
         <div>
             <NavbarComponent />
             <Container style={{ paddingTop: '32px' }}>
-                {userProfile && (
-                    <Row md={2} className="d-flex justify-content-center align-items-center">
-                        <Card className="d-flex flex-column align-items-center" style={{ padding: '16px' }}>
-                            <OverlayTrigger placement="bottom" delay={{ show: 240, hide: 60 }} overlay={handleHoverImage}>
-                                <Card.Img
-                                    variant="top"
-                                    style={{ width: '192px', height: '192px', padding: '8px', borderRadius: '50%', cursor: 'pointer' }}
-                                    src={userProfile.imageProfile}
-                                    alt="Profile Image"
-                                    onClick={handleImageClick}
-                                />
-                            </OverlayTrigger>
-                            <input id="imageInput" type="file" accept=".jpg, .jpeg, .png" style={{ display: 'none' }} onChange={handleImageChange} />
-                            <Card.Body className="d-flex flex-column align-items-center">
-                                <Card.Title><b>{userProfile.username}</b></Card.Title>
-                                <Card.Text>
-                                    <Row>
-                                        <InputGroup className="mt-3">
-                                            <InputGroup.Text id="full-name" style={{ width: '160px' }}>Nama Lengkap</InputGroup.Text>
-                                            <Form.Control id="fullName" type="text" aria-describedby="full-name" value={userProfile.fullName} onChange={handleInputChange} disabled={!isEditMode} />
-                                        </InputGroup>
+                <Row md={2} className="d-flex justify-content-center align-items-center">
+                    <Card className="d-flex flex-column align-items-center" style={{ padding: '16px' }}>
+                        <OverlayTrigger placement="bottom" delay={{ show: 240, hide: 60 }} overlay={handleHoverImage}>
+                            <Card.Img
+                                variant="top"
+                                style={{ width: '192px', height: '192px', padding: '8px', borderRadius: '50%', cursor: 'pointer' }}
+                                src={userProfile.imageProfile}
+                                alt="Profile Image"
+                                onClick={handleImageClick}
+                            />
+                        </OverlayTrigger>
+                        <input id="imageInput" type="file" accept=".jpg, .jpeg, .png" style={{ display: 'none' }} onChange={handleImageChange} />
+                        <Card.Body className="d-flex flex-column align-items-center">
+                            <Card.Title><b>{userProfile.username}</b></Card.Title>
+                            <Card.Text>
+                                <Row>
+                                    <InputGroup className="mt-3">
+                                        <InputGroup.Text id="full-name" style={{ width: '160px', backgroundColor: '#00A47F', color: '#FFFFFF' }}>Nama Lengkap</InputGroup.Text>
+                                        <Form.Control id="fullName" type="text" aria-describedby="full-name" value={userProfile.fullName} onChange={handleInputChange} disabled={!isEditMode} />
+                                    </InputGroup>
 
-                                        <InputGroup className="mt-3">
-                                            <InputGroup.Text id="date-of-birth" style={{ width: '160px' }}>Tanggal Lahir</InputGroup.Text>
-                                            <Form.Control id="dateOfBirth" type="date" aria-describedby="date-of-birth" value={userProfile.dateOfBirth} onChange={handleInputChange} disabled={!isEditMode} />
-                                        </InputGroup>
+                                    <InputGroup className="mt-3">
+                                        <InputGroup.Text id="date-of-birth" style={{ width: '160px', backgroundColor: '#00A47F', color: '#FFFFFF' }}>Tanggal Lahir</InputGroup.Text>
+                                        <Form.Control id="dateOfBirth" type="date" aria-describedby="date-of-birth" value={userProfile.dateOfBirth} onChange={handleInputChange} disabled={!isEditMode} />
+                                    </InputGroup>
 
-                                        <InputGroup className="mt-3">
-                                            <InputGroup.Text id="gender" style={{ width: '160px' }}>Jenis Kelamin</InputGroup.Text>
-                                            <Form.Select id="gender" aria-describedby="gender" value={userProfile.gender} onChange={handleInputChange} disabled={!isEditMode}>
-                                                {genderOptions.map((option) => (
-                                                    <option key={option.value} value={option.value}>{option.label}</option>
-                                                ))}
-                                            </Form.Select>
-                                        </InputGroup>
-
-                                        <InputGroup className="mt-3">
-                                            <InputGroup.Text id="address" style={{ width: '160px' }}>Alamat</InputGroup.Text>
-                                            <Form.Control id="address" as="textarea" aria-describedby="address" value={userProfile.address} onChange={handleInputChange} disabled={!isEditMode} />
-                                        </InputGroup>
-
-                                        <InputGroup className="mt-3">
-                                            <InputGroup.Text id="phone-number" style={{ width: '160px' }}>Nomor Telepon</InputGroup.Text>
-                                            <Form.Control id="phoneNumber" type="number" aria-describedby="phone-number" value={userProfile.phoneNumber} onChange={handleInputChange} disabled={!isEditMode} />
-                                        </InputGroup>
-
-                                        <InputGroup className="mt-3 d-flex justify-content-center align-items-center">
-                                            <InputGroup.Text id="roles">Roles</InputGroup.Text>
-                                            {userProfile.roles.map((roles, index) => (
-                                                <Badge key={index} bg={roles === "talent" ? "warning" : "success"} className="me-1">
-                                                    {roles}
-                                                </Badge>
+                                    <InputGroup className="mt-3">
+                                        <InputGroup.Text id="gender" style={{ width: '160px', backgroundColor: '#00A47F', color: '#FFFFFF' }}>Jenis Kelamin</InputGroup.Text>
+                                        <Form.Select id="gender" aria-describedby="gender" value={userProfile.gender} onChange={handleInputChange} disabled={!isEditMode}>
+                                            {genderOptions.map((option) => (
+                                                <option key={option.value} value={option.value}>{option.label}</option>
                                             ))}
-                                        </InputGroup>
-                                    </Row>
-                                </Card.Text>
-                                {isEditMode ? (
-                                    <Row style={{ marginTop: '32px' }}>
-                                        <Col className='d-flex justify-content-center'>
-                                            <Button variant="secondary" onClick={() => setIsEditMode(false)}>Cancel</Button>
-                                            <Button variant="success" className="ms-2" onClick={handleConfirmClick}>Confirm</Button>
-                                        </Col>
-                                    </Row>
-                                ) : (
-                                    <Row style={{ marginTop: '32px' }}>
-                                        <Col className='d-flex justify-content-center'>
-                                            <Button variant="success" onClick={() => setIsEditMode(true)}>Edit Profile</Button>
-                                        </Col>
-                                    </Row>
-                                )}
-                            </Card.Body>
-                        </Card>
-                    </Row>
-                )}
+                                        </Form.Select>
+                                    </InputGroup>
+
+                                    <InputGroup className="mt-3">
+                                        <InputGroup.Text id="address" style={{ width: '160px', backgroundColor: '#00A47F', color: '#FFFFFF' }}>Alamat</InputGroup.Text>
+                                        <Form.Control id="address" as="textarea" aria-describedby="address" value={userProfile.address} onChange={handleInputChange} disabled={!isEditMode} />
+                                    </InputGroup>
+
+                                    <InputGroup className="mt-3">
+                                        <InputGroup.Text id="phone-number" style={{ width: '160px', backgroundColor: '#00A47F', color: '#FFFFFF' }}>Nomor Telepon</InputGroup.Text>
+                                        <Form.Control id="phoneNumber" type="number" aria-describedby="phone-number" value={userProfile.phoneNumber} onChange={handleInputChange} disabled={!isEditMode} />
+                                    </InputGroup>
+
+                                    <InputGroup className="mt-3 d-flex justify-content-center align-items-center">
+                                        <InputGroup.Text id="roles">Roles</InputGroup.Text>
+                                        {userProfile.roles.map((roles, index) => (
+                                            <Badge key={index} bg={roles === "talent" ? "warning" : "success"} className="me-1">
+                                                {roles}
+                                            </Badge>
+                                        ))}
+                                    </InputGroup>
+                                </Row>
+                            </Card.Text>
+                            {isEditMode ? (
+                                <Row style={{ marginTop: '32px' }}>
+                                    <Col className='d-flex justify-content-center'>
+                                        <Button variant="secondary" onClick={() => setIsEditMode(false)}>Cancel</Button>
+                                        <Button variant="success" className="ms-2" onClick={handleConfirmClick}>Confirm</Button>
+                                    </Col>
+                                </Row>
+                            ) : (
+                                <Row style={{ marginTop: '32px' }}>
+                                    <Col className='d-flex justify-content-center'>
+                                        <Button variant="success" onClick={() => setIsEditMode(true)}>Edit Profile</Button>
+                                    </Col>
+                                </Row>
+                            )}
+                        </Card.Body>
+                    </Card>
+                </Row>
             </Container>
         </div>
     );

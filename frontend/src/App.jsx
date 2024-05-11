1b691217bbdadf3230e2ec410c31dbccc6ff4fa0
DaffaHabibullah <daffahabibullah1@gmail.com>
2024-05-12 00:15:42 +0700

    feat: not found page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Register from './pages/Register';
 import Home from './pages/Home';
 import User from './pages/User';
 import Talent from './pages/Talent';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/user-profile" element={<User />} />
         <Route path="/talent-profile" element={<Talent />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   )
